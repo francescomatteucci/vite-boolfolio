7f60f760edca1aaccf9f1838a06d46c450b6ac74
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-06-12 17:47:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <AppMain/>
+  </div>
+</template>
+
+<script>
+import AppMain from './components/AppMain.vue'
+  export default {
+    components:{
+      AppMain
+    },
+
+    data(){
+      return {
+        message: 'Ciao',
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Posts</h1>
+    </div>
+    <ul>
+        <li class="card" v-for="project in projects">
+            <h2>{{ project.title }}</h2>
+            <p> {{ project.content  }}</p>
+        </li>
+    </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+    export default {
+        data(){
+            return {
+                projects: []
+            }
+        },
+        methods: {
+            fetchPosts() {
+                axios.get('http://127.0.0.1:8000/api/posts').then(res => {
+                    this.projects = res.data.results.data
+                })
+            }
+        },
+        created(){
+            this.fetchPosts()
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
